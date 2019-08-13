384f5552cdae49e41f951e8405f93014059cd1c8
pako herrera <pakman_198@hotmail.com>
2019-08-13 10:24:25 -0700

    Added faker definitions for typescript and used faker to generate random data on User constructor

--- Design Patterns with Typescript/src/User.ts
+++ Design Patterns with Typescript/src/User.ts
@@ -1,7 +1,17 @@
+import faker from "faker";
+
 class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
 }
