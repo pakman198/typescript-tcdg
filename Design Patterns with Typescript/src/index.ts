bd81f35718a556a49b40b6821f87dfcc1a36223f
pako herrera <pakman_198@hotmail.com>
2019-08-14 14:13:58 -0700

    Added Google Maps type definitions

--- Design Patterns with Typescript/src/index.ts
+++ Design Patterns with Typescript/src/index.ts
@@ -1,7 +1,17 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
+// const user = new User();
+// const company = new Company();
 
-console.log({ user, company });
+// console.log({ user, company });
+
+const container = document.getElementById("map");
+
+new google.maps.Map(container, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
