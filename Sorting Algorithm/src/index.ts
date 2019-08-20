050d17064f3a3b84c8f2bc5abb88ccbbb440b44a
pako herrera <pakman_198@hotmail.com>
2019-08-20 09:38:47 -0700

    First implementation of sorting algorithm

---
+++ Sorting Algorithm/src/index.ts
@@ -0,0 +1,21 @@
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort() {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - 1 - i; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
