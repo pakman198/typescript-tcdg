42288ed7839307ecb213b80f9df0a8afdf92280b
pako herrera <pakman_198@hotmail.com>
2019-08-22 10:11:04 -0700

    Created a CharactersCollection class to be able to sort a string. The Sorter class uses an interface Sortable so it can sort different types of data (array of numbers and strings)

--- Sorting Algorithm/src/index.ts
+++ Sorting Algorithm/src/index.ts
@@ -1,7 +1,14 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const collection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(collection);
 sorter.sort();
 console.log(sorter.collection);
+
+const characters = new CharactersCollection("JSAgiut");
+const charactersSorter = new Sorter(characters);
+charactersSorter.sort();
+
+console.log(charactersSorter.collection);
