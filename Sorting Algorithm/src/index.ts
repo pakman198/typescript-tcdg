e7ee834ead372330b1082860f3422522c18f2c6a
pako herrera <pakman_198@hotmail.com>
2019-08-22 15:57:21 -0700

    Created a class to srto a LinkedList

--- Sorting Algorithm/src/index.ts
+++ Sorting Algorithm/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const collection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(collection);
@@ -12,3 +13,14 @@ const charactersSorter = new Sorter(characters);
 charactersSorter.sort();
 
 console.log(charactersSorter.collection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(200);
+linkedList.add(-100);
+linkedList.add(0);
+linkedList.add(-50);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
