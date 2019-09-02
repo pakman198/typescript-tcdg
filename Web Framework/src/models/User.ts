7685fb29a878f2c47083d4f0e4fc41fd0c367585
Pako Herrera <pakman_198@hotmail.com>
2019-09-02 14:45:25 -0700

    Created a user model with get and set properties

---
+++ Web Framework/src/models/User.ts
@@ -0,0 +1,16 @@
+export interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+}
