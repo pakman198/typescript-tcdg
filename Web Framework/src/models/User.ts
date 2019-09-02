a4fa7e12cfafe8331fb794ed3981c39ee33b3f5e
Pako Herrera <pakman_198@hotmail.com>
2019-09-02 15:05:22 -0700

    Added an eventing system to the class

--- Web Framework/src/models/User.ts
+++ Web Framework/src/models/User.ts
@@ -1,9 +1,14 @@
-export interface UserProps {
+interface UserProps {
   name?: string;
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+
+  events: { [key: string]: Callback[] };
+
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
@@ -12,5 +17,23 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if( !handlers || handlers.length === 0) {
+      return; 
+    }
+
+    handlers.forEach(callback => callback());
+
   }
 }
