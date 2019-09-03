d2c219c978840079fce5ad1ba8b45d87c909d28c
Pako Herrera <pakman_198@hotmail.com>
2019-09-03 10:09:42 -0700

    Got all the functionality running on the User class

--- Web Framework/src/models/Eventing.ts
+++ Web Framework/src/models/Eventing.ts
@@ -2,8 +2,8 @@ type Callback = () => void;
 
 export class Eventing {
   events: { [key: string]: Callback[] };
-  
-  on(eventName: string, callback: Callback): void {
+
+  on = (eventName: string, callback: Callback): void => {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
 
--- Web Framework/src/models/User.ts
+++ Web Framework/src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
+import { AxiosResponse } from 'axios';
 
 export interface UserProps {
   id?: number;
@@ -18,5 +19,35 @@ export class User {
 
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.set;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+
+    if(typeof id !== 'number') {
+      throw new Error("Can't fetch without an id");
+    }
+
+    this.sync.fetch(id)
+    .then((response: AxiosResponse): void => {
+      this.attributes.set(response.data);
+    })
   }
 }
